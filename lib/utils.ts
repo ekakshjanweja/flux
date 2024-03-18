573cc2eb9c870e27f3d2a3035486172872fbf35a
ekaksh <jekaksh@gmail.com>
2024-03-18 17:13:33 +0530

    Created Cursor Presence

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,34 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Camera } from "@/types/canvas";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const COLORS = [
+  "#DC2626",
+  "#059669",
+  "#2563EB",
+  "#6D28D9",
+  "#0EA5E9",
+  "#818CF8",
+  "#DB2777",
+  "#dc7526",
+  "#afdc26",
+  "#26dc7b",
+];
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
+}
+
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera
+) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
 }
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -3,6 +3,7 @@ import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
 
 const client = createClient({
   authEndpoint: "/api/liveblocks-auth",
+  throttle: 16,
   // authEndpoint: "/api/liveblocks-auth",
   // throttle: 100,
   async resolveUsers({ userIds }) {
@@ -51,8 +52,7 @@ const client = createClient({
 // and that will automatically be kept in sync. Accessible through the
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
-  // cursor: { x: number, y: number } | null,
-  // ...
+  cursor: { x: number; y: number } | null;
 };
 
 // Optionally, Storage represents the shared document that persists in the

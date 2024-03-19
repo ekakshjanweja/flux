b695b33379955ce494ca9b9dcee0ea7d31776ae5
ekaksh <jekaksh@gmail.com>
2024-03-19 22:09:34 +0530

    Insert Layer Rectangle

---
+++ app/board/[boardId]/_components/rectangle.tsx
@@ -0,0 +1,38 @@
+import { RectangleLayer } from "@/types/canvas";
+
+interface RectanlgeProps {
+  id: string;
+  layer: RectangleLayer;
+  onPointerDown: (e: React.PointerEvent, layerId: string) => void;
+  selectionColor?: string;
+}
+
+export const Rectangle = ({
+  id,
+  layer,
+  onPointerDown,
+  selectionColor,
+}: RectanlgeProps) => {
+  const { x, y, height, width, fill } = layer;
+
+  return (
+    <>
+      <rect
+        className="drop-shadow-md"
+        onPointerDown={(e) => {
+          onPointerDown(e, id);
+        }}
+        style={{
+          transform: `translate(${x}px, ${y}px)`,
+        }}
+        x={0}
+        y={0}
+        width={width}
+        height={height}
+        strokeWidth={1}
+        stroke="transparent"
+        fill="#FFFFFF"
+      />
+    </>
+  );
+};
